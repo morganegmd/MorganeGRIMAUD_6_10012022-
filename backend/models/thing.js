32befe6e1acbad4a1c8617b0339759586809f30f
Morgane GRIMAUD <morganegmd@MacBook-Air-de-macbook.local>
2022-01-29 20:14:36 +0100

    error new sauce

--- backend/models/thing.js
+++ backend/models/thing.js
@@ -4,9 +4,13 @@ const mongoose = require("mongoose");
 //Création du schéma
 
 const thingSchema = mongoose.Schema({
-  firstName: { type: String, required: true },
+  name: { type: String, required: true },
+  userId: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  ProfilePicture: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  ingredient: { type: String, required: true },
   // sans le true, l'article ne sera pas sauvegardé, Pas besoin de mettre un champ pour l'Id puisqu'il est automatiquement généré par Mongoose ;
 });
 
